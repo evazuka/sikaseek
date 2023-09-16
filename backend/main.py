80184eae20c9d0b70dc5bdb0052850318f19f856
EZM <eva.zhuk@gmail.com>
2023-09-16 12:50:49 +0200

    added search in the scope of specified context

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.prompts import PromptTemplate
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+text_qa_template_str = (
+    "Context information is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Using only context information answer the question: {query_str}\n"
+)
+text_qa_template = PromptTemplate(text_qa_template_str)
+
+refine_template_str = (
+    "The original question is as follows: {query_str}\n"
+    "We have provided an existing answer: {existing_answer}\n"
+    "We have the opportunity to refine the existing answer "
+    "(only if needed) with some more context below.\n"
+    "------------\n"
+    "{context_msg}\n"
+    "------------\n"
+    "Using both the new context and the old context, update or repeat the existing answer.\n"
+)
+refine_template = PromptTemplate(refine_template_str)
 
 
 # GPT doesn't find the corresponding evidence in the leaf node, but still gives the correct answer
@@ -33,6 +55,5 @@ def read_root():
 
 @app.get("/{query}")
 def read_item(query: str, filter: Union[str, None] = None):
-    return query_engine.query(
-    query
-)
+    return index.as_query_engine(text_qa_template=text_qa_template, refine_template=refine_template
+    ).query(query)
