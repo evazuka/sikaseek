87aaac13755e9feead26bc306cb7349ff1292f54
EZM <eva.zhuk@gmail.com>
2023-09-16 13:09:10 +0200

    amended API

--- backend/main.py
+++ backend/main.py
@@ -20,22 +20,10 @@ text_qa_template_str = (
     "---------------------\n"
     "{context_str}\n"
     "---------------------\n"
-    "Using only context information answer the question: {query_str}\n"
+    "Using context information answer the question: {query_str}\n"
+    "If the context isn't helpful, you can also answer the question using your own knowledge base.\n"
 )
 text_qa_template = PromptTemplate(text_qa_template_str)
-
-refine_template_str = (
-    "The original question is as follows: {query_str}\n"
-    "We have provided an existing answer: {existing_answer}\n"
-    "We have the opportunity to refine the existing answer "
-    "(only if needed) with some more context below.\n"
-    "------------\n"
-    "{context_msg}\n"
-    "------------\n"
-    "Using both the new context and the old context, update or repeat the existing answer.\n"
-)
-refine_template = PromptTemplate(refine_template_str)
-
 
 # GPT doesn't find the corresponding evidence in the leaf node, but still gives the correct answer
 # set Logging to DEBUG for more detailed outputs
@@ -55,5 +43,5 @@ def read_root():
 
 @app.get("/{query}")
 def read_item(query: str, filter: Union[str, None] = None):
-    return index.as_query_engine(text_qa_template=text_qa_template, refine_template=refine_template
+    return index.as_query_engine(text_qa_template=text_qa_template
     ).query(query)
