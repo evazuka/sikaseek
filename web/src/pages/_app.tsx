96168b486f3e6d4c35c515ba8c5f3b8671d032aa
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2023-09-16 09:45:56 +0200

    Tree context

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,9 +3,12 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 };
 
 export default api.withTRPC(MyApp);
