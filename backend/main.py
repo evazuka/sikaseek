7e89697fe4614e41954002b88ff7790886396b5a
EZM <eva.zhuk@gmail.com>
2023-09-16 10:59:22 +0200

    amended API

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
+import logging
+import sys
+
 from typing import Union
 
 from fastapi import FastAPI
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+# GPT doesn't find the corresponding evidence in the leaf node, but still gives the correct answer
+# set Logging to DEBUG for more detailed outputs
+storage_context = StorageContext.from_defaults(persist_dir="./storage-vector")
+# load index
+index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
 
 app = FastAPI()
 
@@ -12,4 +33,6 @@ def read_root():
 
 @app.get("/{query}")
 def read_item(query: str, filter: Union[str, None] = None):
-    return {"query": query, "filter": filter}
+    return query_engine.query(
+    query
+)
